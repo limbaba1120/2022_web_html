4838295a4d236b09ac6ea4e5524b539e3beafeaf
limbaba1120 <kwlim1120ss@gmail.com>
2023-08-09 11:19:22 +0900

    web HTML

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".home-slider", {
+    loop:true, 
+    grabCursor:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
